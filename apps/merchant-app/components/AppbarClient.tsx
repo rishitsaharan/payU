aac62886e88d445be47bf56ed380af5b96c885c3
Rishit Saharan <rishit19382@iiitd.ac.in>
2024-04-10 12:17:17 +0530

    reset

--- apps/merchant-app/components/AppbarClient.tsx
+++ apps/merchant-app/components/AppbarClient.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { AppBar } from "@repo/ui/appbar";
-import { signIn, signOut, useSession } from "next-auth/react"
+import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 export const AppbarClient = () => {
