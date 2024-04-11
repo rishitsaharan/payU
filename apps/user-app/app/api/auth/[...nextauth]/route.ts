e41aa220aff47027be3dceae48dd45481a11f1d8
Rishit Saharan <rishit19382@iiitd.ac.in>
2024-04-11 11:47:37 +0530

    update

--- apps/user-app/app/api/auth/[...nextauth]/route.ts
+++ apps/user-app/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import { authOptions } from "../../../lib/auth"
 
+console.log(process.env.NEXTAUTH_URL+" "+process.env.NEXTAUTH_URL_USER);
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
