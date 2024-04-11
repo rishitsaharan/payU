a8e35566c6f2511d5bc2a82afdc175562b87ad57
Rishit Saharan <rishit19382@iiitd.ac.in>
2024-04-11 12:20:07 +0530

    update

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
-  env : {
-    NEXTAUTH_URL:"http://13.60.57.102:3000",
-  }
+  // env : {
+  //   NEXTAUTH_URL:"http://13.60.57.102:3000",
+  // }
 };
